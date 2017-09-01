a036f2e22e362dd1230248c3f686956b1e4bcb14
yollysparks <yolanda.owiso.yo@gmail.com>
2017-09-01 09:44:16 +0200

    Merge origin/master
    Conflicts:
    	pom.xml
    	src/main/java/Controller/ConsumerServlet.java
    	src/main/java/Controller/CustomerServlet.java
    	src/main/java/Data/CustomerMapper.java
    	src/main/java/testing.java
    	src/main/webapp/WEB-INF/web.xml
    	src/test/java/Data/CustomerMapperTest.java

---
+++ src/main/webapp/app.js
@@ -0,0 +1,44 @@
+var xmlHttp;
+function srvTime(){
+    try {
+        //FF, Opera, Safari, Chrome
+        xmlHttp = new XMLHttpRequest();
+    }
+    catch (err1) {
+        //IE
+        try {
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        }
+        catch (err2) {
+            try {
+                xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            catch (eerr3) {
+                //AJAX not supported, use CPU time.
+                alert("AJAX not supported");
+            }
+        }
+    }
+    xmlHttp.open('HEAD',window.location.href.toString(),false);
+    xmlHttp.setRequestHeader("Content-Type", "text/html");
+    xmlHttp.send('');
+    return xmlHttp.getResponseHeader("Date");
+}
+var st = srvTime();
+var date = new Date(st);
+
+function clickHandler()
+{
+    var promise = fetch('ServerTimeServlet');
+     promise.then(function (response) {
+       return response.json();
+     }).then(function (persons) {
+        
+   var localTime = new Date();
+  document.write("Local machine time is: " + localTime + "<br>");
+  document.write("Server time is: " + date);
+  document.getElementById("btn").onclick = clickHandler;
+
+ });
+}
+
